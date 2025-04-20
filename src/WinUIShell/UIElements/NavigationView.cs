26da76f0e4b046b7801652892af3ee17b192c0cd
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2025-04-20 16:33:17 +0900

    Add NavigationView

---
+++ src/WinUIShell/UIElements/NavigationView.cs
@@ -0,0 +1,111 @@
+﻿using WinUIShell.Common;
+
+namespace WinUIShell;
+
+public class NavigationView : ContentControl
+{
+    public bool AlwaysShowHeader
+    {
+        get => PropertyAccessor.Get<bool>(Id, nameof(AlwaysShowHeader))!;
+        set => PropertyAccessor.Set(Id, nameof(AlwaysShowHeader), value);
+    }
+
+    //public AutoSuggestBox AutoSuggestBox
+
+    public double CompactModeThresholdWidth
+    {
+        get => PropertyAccessor.Get<double>(Id, nameof(CompactModeThresholdWidth))!;
+        set => PropertyAccessor.Set(Id, nameof(CompactModeThresholdWidth), value);
+    }
+
+    public double CompactPaneLength
+    {
+        get => PropertyAccessor.Get<double>(Id, nameof(CompactPaneLength))!;
+        set => PropertyAccessor.Set(Id, nameof(CompactPaneLength), value);
+    }
+
+    //public UIElement ContentOverlay
+    //public NavigationViewDisplayMode DisplayMode => INavigationViewMethods.get_DisplayMode(_objRef_global__Microsoft_UI_Xaml_Controls_INavigationView);
+
+    public double ExpandedModeThresholdWidth
+    {
+        get => PropertyAccessor.Get<double>(Id, nameof(ExpandedModeThresholdWidth))!;
+        set => PropertyAccessor.Set(Id, nameof(ExpandedModeThresholdWidth), value);
+    }
+
+    //public IList<object> FooterMenuItems => INavigationViewMethods.get_FooterMenuItems(_objRef_global__Microsoft_UI_Xaml_Controls_INavigationView);
+    //public object FooterMenuItemsSource
+
+    public object? Header
+    {
+        get => PropertyAccessor.Get<object>(Id, nameof(Header));
+        set
+        {
+            if (value is WinUIShellObject v)
+            {
+                PropertyAccessor.Set(Id, nameof(Header), v.Id);
+            }
+            else
+            {
+                PropertyAccessor.Set(Id, nameof(Header), value);
+            }
+        }
+    }
+
+    //public DataTemplate HeaderTemplate
+    //public NavigationViewBackButtonVisible IsBackButtonVisible
+    //public bool IsBackEnabled
+    //public bool IsPaneOpen
+    //public bool IsPaneToggleButtonVisible
+    //public bool IsPaneVisible
+    //public bool IsSettingsVisible
+    //public bool IsTitleBarAutoPaddingEnabled
+    //public Style MenuItemContainerStyle
+    //public StyleSelector MenuItemContainerStyleSelector
+    //public DataTemplate MenuItemTemplate
+    //public DataTemplateSelector MenuItemTemplateSelector
+    //public IList<object> MenuItems => INavigationViewMethods.get_MenuItems(_objRef_global__Microsoft_UI_Xaml_Controls_INavigationView)
+    //public object MenuItemsSource
+    //public double OpenPaneLength
+    //public NavigationViewOverflowLabelMode OverflowLabelMode
+    //public UIElement PaneCustomContent
+    //public NavigationViewPaneDisplayMode PaneDisplayMode
+    //public UIElement PaneFooter
+    //public UIElement PaneHeader
+
+    public string PaneTitle
+    {
+        get => PropertyAccessor.Get<string>(Id, nameof(PaneTitle))!;
+        set => PropertyAccessor.Set(Id, nameof(PaneTitle), value);
+    }
+
+    //public Style PaneToggleButtonStyle
+
+    public object? SelectedItem
+    {
+        get => PropertyAccessor.Get<object>(Id, nameof(SelectedItem));
+        set
+        {
+            if (value is WinUIShellObject v)
+            {
+                PropertyAccessor.Set(Id, nameof(SelectedItem), v.Id);
+            }
+            else
+            {
+                PropertyAccessor.Set(Id, nameof(SelectedItem), value);
+            }
+        }
+    }
+
+    //public NavigationViewSelectionFollowsFocus SelectionFollowsFocus
+    //public object SettingsItem => INavigationViewMethods.get_SettingsItem(_objRef_global__Microsoft_UI_Xaml_Controls_INavigationView);
+    //public NavigationViewShoulderNavigationEnabled ShoulderNavigationEnabled
+    //public NavigationViewTemplateSettings TemplateSettings => INavigationView2Methods.get_TemplateSettings(_objRef_global__Microsoft_UI_Xaml_Controls_INavigationView2);
+
+    public NavigationView()
+    {
+        Id = CommandClient.Get().CreateObject(
+            "Microsoft.UI.Xaml.Controls.NavigationView, Microsoft.WinUI",
+            this);
+    }
+}
