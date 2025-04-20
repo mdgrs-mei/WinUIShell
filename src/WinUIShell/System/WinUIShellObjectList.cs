70e226a5c44e7128dd42cacbb22d78769ccd6809
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2025-04-20 21:02:05 +0900

    Add WinUIShellObjectList and use it for UIElementCollection

---
+++ src/WinUIShell/System/WinUIShellObjectList.cs
@@ -0,0 +1,97 @@
+﻿using WinUIShell.Common;
+
+namespace WinUIShell;
+
+public class WinUIShellObjectList<T> : WinUIShellObject
+{
+    public int Count
+    {
+        get => PropertyAccessor.Get<int>(Id, nameof(Count))!;
+    }
+
+    public bool IsReadOnly
+    {
+        get => PropertyAccessor.Get<bool>(Id, nameof(IsReadOnly))!;
+    }
+
+    public T this[int index]
+    {
+        get => PropertyAccessor.GetIndexer<T>(Id, index)!;
+        set => PropertyAccessor.SetIndexer(Id, index, value);
+    }
+
+    internal WinUIShellObjectList(ObjectId id)
+        : base(id)
+    {
+    }
+
+    public int IndexOf(T item)
+    {
+        if (item is WinUIShellObject obj)
+        {
+            return CommandClient.Get().InvokeMethodAndGetResult<int>(Id, nameof(IndexOf), obj.Id);
+        }
+        else
+        {
+            return CommandClient.Get().InvokeMethodAndGetResult<int>(Id, nameof(IndexOf), item);
+        }
+    }
+
+    public void Insert(int index, T item)
+    {
+        if (item is WinUIShellObject obj)
+        {
+            CommandClient.Get().InvokeMethod(Id, nameof(Insert), index, obj.Id);
+        }
+        else
+        {
+            CommandClient.Get().InvokeMethod(Id, nameof(Insert), index, item);
+        }
+    }
+
+    public void RemoveAt(int index)
+    {
+        CommandClient.Get().InvokeMethod(Id, nameof(RemoveAt), index);
+    }
+
+    public void Add(T item)
+    {
+        if (item is WinUIShellObject obj)
+        {
+            CommandClient.Get().InvokeMethod(Id, nameof(Add), obj.Id);
+        }
+        else
+        {
+            CommandClient.Get().InvokeMethod(Id, nameof(Add), item);
+        }
+    }
+
+    public void Clear()
+    {
+        CommandClient.Get().InvokeMethod(Id, nameof(Clear));
+    }
+
+    public bool Contains(T item)
+    {
+        if (item is WinUIShellObject obj)
+        {
+            return CommandClient.Get().InvokeMethodAndGetResult<bool>(Id, nameof(Contains), obj.Id);
+        }
+        else
+        {
+            return CommandClient.Get().InvokeMethodAndGetResult<bool>(Id, nameof(Contains), item);
+        }
+    }
+
+    public bool Remove(T item)
+    {
+        if (item is WinUIShellObject obj)
+        {
+            return CommandClient.Get().InvokeMethodAndGetResult<bool>(Id, nameof(Remove), obj.Id);
+        }
+        else
+        {
+            return CommandClient.Get().InvokeMethodAndGetResult<bool>(Id, nameof(Remove), item);
+        }
+    }
+}
--- src/WinUIShell/UIElements/UIElementCollection.cs
+++ src/WinUIShell/UIElements/UIElementCollection.cs
@@ -2,24 +2,8 @@
 
 namespace WinUIShell;
 
-public class UIElementCollection : WinUIShellObject
+public class UIElementCollection : WinUIShellObjectList<UIElement>
 {
-    public int Count
-    {
-        get => PropertyAccessor.Get<int>(Id, nameof(Count))!;
-    }
-
-    public bool IsReadOnly
-    {
-        get => PropertyAccessor.Get<bool>(Id, nameof(IsReadOnly))!;
-    }
-
-    public UIElement this[int index]
-    {
-        get => PropertyAccessor.GetIndexer<UIElement>(Id, index)!;
-        set => PropertyAccessor.SetIndexer(Id, index, value);
-    }
-
     internal UIElementCollection(ObjectId id)
         : base(id)
     {
@@ -28,40 +12,5 @@ public class UIElementCollection : WinUIShellObject
     public void Move(uint oldIndex, uint newIndex)
     {
         CommandClient.Get().InvokeMethod(Id, nameof(Move), oldIndex, newIndex);
-    }
-
-    public int IndexOf(UIElement item)
-    {
-        return CommandClient.Get().InvokeMethodAndGetResult<int>(Id, nameof(IndexOf), item?.Id);
-    }
-
-    public void Insert(int index, UIElement item)
-    {
-        CommandClient.Get().InvokeMethod(Id, nameof(Insert), index, item?.Id);
-    }
-
-    public void RemoveAt(int index)
-    {
-        CommandClient.Get().InvokeMethod(Id, nameof(RemoveAt), index);
-    }
-
-    public void Add(UIElement item)
-    {
-        CommandClient.Get().InvokeMethod(Id, nameof(Add), item?.Id);
-    }
-
-    public void Clear()
-    {
-        CommandClient.Get().InvokeMethod(Id, nameof(Clear));
-    }
-
-    public bool Contains(UIElement item)
-    {
-        return CommandClient.Get().InvokeMethodAndGetResult<bool>(Id, nameof(Contains), item?.Id);
-    }
-
-    public bool Remove(UIElement item)
-    {
-        return CommandClient.Get().InvokeMethodAndGetResult<bool>(Id, nameof(Remove), item?.Id);
     }
 }
