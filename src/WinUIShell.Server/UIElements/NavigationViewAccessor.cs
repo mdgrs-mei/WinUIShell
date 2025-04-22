a18ef1a9a96a4e0962f746d98755c87c5960028f
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2025-04-22 22:31:41 +0900

    Add NavigationViewItemInvoked event

---
+++ src/WinUIShell.Server/UIElements/NavigationViewAccessor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Windows.Foundation;
+
+namespace WinUIShell.Server;
+internal static class NavigationViewAccessor
+{
+    public static void AddItemInvoked(
+        NavigationView target,
+        int queueThreadId,
+        string eventListId,
+        int eventId,
+        object?[]? disabledControlsWhileProcessing)
+    {
+        var callback = EventCallback.Create<NavigationViewItemInvokedEventArgs>(
+            queueThreadId,
+            eventListId,
+            eventId,
+            disabledControlsWhileProcessing);
+
+        target.ItemInvoked += new TypedEventHandler<NavigationView, NavigationViewItemInvokedEventArgs>(callback);
+    }
+}
---
+++ src/WinUIShell/UIElements/NavigationViewItemInvokedEventArgs.cs
@@ -0,0 +1,40 @@
+﻿using WinUIShell.Common;
+
+namespace WinUIShell;
+
+public class NavigationViewItemInvokedEventArgs
+{
+    public object? InvokedItem
+    {
+        get
+        {
+            var id = ObjectStore.Get().GetId(this);
+            return PropertyAccessor.Get<object>(id, nameof(InvokedItem));
+        }
+    }
+
+    public NavigationViewItemBase? InvokedItemContainer
+    {
+        get
+        {
+            var id = ObjectStore.Get().GetId(this);
+            return PropertyAccessor.Get<NavigationViewItemBase>(id, nameof(InvokedItemContainer));
+        }
+    }
+
+    public bool IsSettingsInvoked
+    {
+        get
+        {
+            var id = ObjectStore.Get().GetId(this);
+            return PropertyAccessor.Get<bool>(id, nameof(IsSettingsInvoked))!;
+        }
+        set
+        {
+            var id = ObjectStore.Get().GetId(this);
+            PropertyAccessor.SetAndWait(id, nameof(IsSettingsInvoked), value);
+        }
+    }
+
+    //public NavigationTransitionInfo RecommendedNavigationTransitionInfo => INavigationViewItemInvokedEventArgs2Methods.get_RecommendedNavigationTransitionInfo(_objRef_global__Microsoft_UI_Xaml_Controls_INavigationViewItemInvokedEventArgs2);
+}
