3698d4b4eb39b80830172b0f0fbab0d47c40fc97
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2025-06-28 21:48:26 +0900

    Add ObjectTypeMapping

---
+++ src/WinUIShell.Common/ObjectTypeMapping.cs
@@ -0,0 +1,58 @@
+﻿namespace WinUIShell.Common;
+
+internal sealed class ObjectTypeMapping : Singleton<EnumTypeMapping>
+{
+    private readonly Dictionary<string, string> _map = [];
+
+    private readonly List<(string, string)> _list = [
+        ("WinUIShell.Application, WinUIShell", "Microsoft.UI.Xaml.Application, Microsoft.WinUI"),
+        ("WinUIShell.Button, WinUIShell", "Microsoft.UI.Xaml.Controls.Button, Microsoft.WinUI"),
+        ("WinUIShell.ColumnDefinition, WinUIShell", "Microsoft.UI.Xaml.Controls.ColumnDefinition, Microsoft.WinUI"),
+        ("WinUIShell.CornerRadius, WinUIShell", "Microsoft.UI.Xaml.CornerRadius, Microsoft.WinUI"),
+        ("WinUIShell.DesktopAcrylicBackdrop, WinUIShell", "Microsoft.UI.Xaml.Media.DesktopAcrylicBackdrop, Microsoft.WinUI"),
+        ("WinUIShell.DrillInNavigationTransitionInfo, WinUIShell", "Microsoft.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo, Microsoft.WinUI"),
+        ("WinUIShell.EntranceNavigationTransitionInfo, WinUIShell", "Microsoft.UI.Xaml.Media.Animation.EntranceNavigationTransitionInfo, Microsoft.WinUI"),
+        ("WinUIShell.FontFamily, WinUIShell", "Microsoft.UI.Xaml.Media.FontFamily, Microsoft.WinUI"),
+        ("WinUIShell.FontWeight, WinUIShell", "Windows.UI.Text.FontWeight, Microsoft.Windows.SDK.NET"),
+        ("WinUIShell.Frame, WinUIShell", "Microsoft.UI.Xaml.Controls.Frame, Microsoft.WinUI"),
+        ("WinUIShell.Grid, WinUIShell", "Microsoft.UI.Xaml.Controls.Grid, Microsoft.WinUI"),
+        ("WinUIShell.GridLength, WinUIShell", "Microsoft.UI.Xaml.GridLength, Microsoft.WinUI"),
+        ("WinUIShell.HyperlinkButton, WinUIShell", "Microsoft.UI.Xaml.Controls.HyperlinkButton, Microsoft.WinUI"),
+        ("WinUIShell.MicaBackdrop, WinUIShell", "Microsoft.UI.Xaml.Media.MicaBackdrop, Microsoft.WinUI"),
+        ("WinUIShell.NavigationView, WinUIShell", "Microsoft.UI.Xaml.Controls.NavigationView, Microsoft.WinUI"),
+        ("WinUIShell.NavigationViewItem, WinUIShell", "Microsoft.UI.Xaml.Controls.NavigationViewItem, Microsoft.WinUI"),
+        ("WinUIShell.NavigationViewItemHeader, WinUIShell", "Microsoft.UI.Xaml.Controls.NavigationViewItemHeader, Microsoft.WinUI"),
+        ("WinUIShell.NavigationViewItemSeparator, WinUIShell", "Microsoft.UI.Xaml.Controls.NavigationViewItemSeparator, Microsoft.WinUI"),
+        ("WinUIShell.PasswordBox, WinUIShell", "Microsoft.UI.Xaml.Controls.PasswordBox, Microsoft.WinUI"),
+        ("WinUIShell.ProgressBar, WinUIShell", "Microsoft.UI.Xaml.Controls.ProgressBar, Microsoft.WinUI"),
+        ("WinUIShell.ProgressRing, WinUIShell", "Microsoft.UI.Xaml.Controls.ProgressRing, Microsoft.WinUI"),
+        ("WinUIShell.RowDefinition, WinUIShell", "Microsoft.UI.Xaml.Controls.RowDefinition, Microsoft.WinUI"),
+        ("WinUIShell.ScrollView, WinUIShell", "Microsoft.UI.Xaml.Controls.ScrollView, Microsoft.WinUI"),
+        ("WinUIShell.SlideNavigationTransitionInfo, WinUIShell", "Microsoft.UI.Xaml.Media.Animation.SlideNavigationTransitionInfo, Microsoft.WinUI"),
+        ("WinUIShell.SolidColorBrush, WinUIShell", "Microsoft.UI.Xaml.Media.SolidColorBrush, Microsoft.WinUI"),
+        ("WinUIShell.StackPanel, WinUIShell", "Microsoft.UI.Xaml.Controls.StackPanel, Microsoft.WinUI"),
+        ("WinUIShell.SymbolIcon, WinUIShell", "Microsoft.UI.Xaml.Controls.SymbolIcon, Microsoft.WinUI"),
+        ("WinUIShell.SymbolIconSource, WinUIShell", "Microsoft.UI.Xaml.Controls.SymbolIconSource, Microsoft.WinUI"),
+        ("WinUIShell.TextBlock, WinUIShell", "Microsoft.UI.Xaml.Controls.TextBlock, Microsoft.WinUI"),
+        ("WinUIShell.TextBox, WinUIShell", "Microsoft.UI.Xaml.Controls.TextBox, Microsoft.WinUI"),
+        ("WinUIShell.Thickness, WinUIShell", "Microsoft.UI.Xaml.Thickness, Microsoft.WinUI"),
+        ("WinUIShell.TitleBar, WinUIShell", "Microsoft.UI.Xaml.Controls.TitleBar, Microsoft.WinUI"),
+        ("WinUIShell.ToggleSwitch, WinUIShell", "Microsoft.UI.Xaml.Controls.ToggleSwitch, Microsoft.WinUI"),
+        ("WinUIShell.Uri, WinUIShell", "System.Uri, System.Private.Uri"),
+        ("WinUIShell.Window, WinUIShell", "Microsoft.UI.Xaml.Window, Microsoft.WinUI"),
+    ];
+
+    public ObjectTypeMapping()
+    {
+        foreach (var map in _list)
+        {
+            _map.Add(map.Item1, map.Item2);
+            _map.Add(map.Item2, map.Item1);
+        }
+    }
+
+    public bool TryGetValue(string sourceEnumType, out string? targetEnumType)
+    {
+        return _map.TryGetValue(sourceEnumType, out targetEnumType);
+    }
+}
