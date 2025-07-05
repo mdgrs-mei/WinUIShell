eb9b80109519d5372000e788ba4bd5280126bc25
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2025-07-05 20:47:27 +0900

    Add XamlReader

---
+++ src/WinUIShell/UIElements/XamlReader.cs
@@ -0,0 +1,14 @@
+﻿using WinUIShell.Common;
+
+namespace WinUIShell;
+
+public static class XamlReader
+{
+    public static object? Load(string xaml)
+    {
+        return CommandClient.Get().InvokeStaticMethodAndGetResult<object>(
+                    "Microsoft.UI.Xaml.Markup.XamlReader, Microsoft.WinUI",
+                    nameof(Load),
+                    xaml);
+    }
+}
