c8f99eecef79cf1f9ecfebb19815de474ac838c7
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2025-07-29 22:04:58 +0900

    Add reflection based event subscription

--- src/WinUIShell.Server/System/EventCallback.cs
+++ src/WinUIShell.Server/System/EventCallback.cs
@@ -1,8 +1,51 @@
-﻿using WinUIShell.Common;
+﻿using System.Reflection;
+using WinUIShell.Common;
 namespace WinUIShell.Server;
 
 internal static class EventCallback
 {
+    public static void Add(
+        object target,
+        string eventName,
+        string eventArgsTypeName,
+        int queueThreadId,
+        string eventListId,
+        int eventId,
+        object?[]? disabledControlsWhileProcessing)
+    {
+        var targetType = target.GetType();
+
+        var eventInfo = targetType.GetEvent(eventName);
+        if (eventInfo is null)
+        {
+            throw new InvalidOperationException($"Event [{eventName}] not found in [{targetType.Name}].");
+        }
+
+        var eventArgsType = Type.GetType(eventArgsTypeName);
+        if (eventArgsType is null)
+        {
+            throw new InvalidOperationException($"Type [{eventArgsTypeName}] not found.");
+        }
+
+        var callbackCreatorGeneric = typeof(EventCallback).GetMethod("Create", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)!;
+        var callbackCreator = callbackCreatorGeneric.MakeGenericMethod(eventArgsType);
+
+        var callback = callbackCreator.Invoke(null, [
+            queueThreadId,
+            eventListId,
+            eventId,
+            disabledControlsWhileProcessing])!;
+
+        var callbackType = callback.GetType();
+        var callbackTargetProperty = callbackType.GetProperty("Target", BindingFlags.Instance | BindingFlags.Public)!;
+        var callbackTarget = callbackTargetProperty.GetValue(callback)!;
+        var callbackMethodInfoProperty = callbackType.GetProperty("Method", BindingFlags.Instance | BindingFlags.Public)!;
+        var callbackMethodInfo = (MethodInfo)callbackMethodInfoProperty.GetValue(callback)!;
+
+        var handler = Delegate.CreateDelegate(eventInfo.EventHandlerType!, callbackTarget, callbackMethodInfo);
+        eventInfo.AddEventHandler(target, handler);
+    }
+
     public static Action<object, TEventArgs> Create<TEventArgs>(
         int queueThreadId,
         string eventListId,
