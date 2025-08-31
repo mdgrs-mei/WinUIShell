9a3cff4986d8764874d002f95ebf107f6a89c8a1
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2025-08-31 14:14:33 +0900

    Refactor MultipleProgressBars example

--- examples/MultipleProgressBars.ps1
+++ examples/MultipleProgressBars.ps1
@@ -1,46 +1,63 @@
 # See CancelLongRunningEventCallback.ps1 first for basic usage of runspace pool event callbacks.
 
 using namespace WinUIShell
-if (-not (Get-Module WinUIShell)) {
-    Import-Module WinUIShell
-}
-
-# This script runs in the global scope of each runspace in the runspace pool.
-$runspaceInitializationScript = {
-    param ($ScriptRoot)
-    "ScriptRoot: $ScriptRoot Runspace:$([runspace]::DefaultRunspace.id)" | Write-Host
-
-    # Functions and variables defined here become available in each runspace.
-    function SetPause($SyncHash, $IsPaused) {
-        $SyncHash.isPaused = $IsPaused
-        if ($IsPaused) {
-            $SyncHash.pauseButton.Content = $SyncHash.playIcon
-        } else {
-            $SyncHash.pauseButton.Content = $SyncHash.pauseIcon
-        }
-    }
-}
-
-# RunspaceCount defines the number of event callbacks that can run in parallel at a time.
-Set-WUIRunspacePoolOption -RunspaceCount 3 -InitializationScript $runspaceInitializationScript -InitializationScriptArgumentList $PSScriptRoot
-
-# Dot source the initialization script here to make the functions available in the default runspace too.
-. $runspaceInitializationScript $PSScriptRoot
 
 $resources = [Application]::Current.Resources
-$win = [Window]::new()
-$win.Title = 'Multiple ProgressBars'
-$win.AppWindow.TitleBar.PreferredTheme = 'UseDefaultAppMode'
-$win.AppWindow.ResizeClient(420, 200)
-
 $syncHashList = @()
-$win.AddClosed({
-        # $win.WaitForClosed() waits until all the event callbacks associated with the window finish so
-        # clear the pause state to resume the event callbacks.
-        foreach ($syncHash in $syncHashList) {
-            SetPause $syncHash $false
+
+function Main() {
+    if (-not (Get-Module WinUIShell)) {
+        Import-Module WinUIShell
+    }
+
+    # This script runs in the global scope of each runspace in the runspace pool.
+    $runspaceInitializationScript = {
+        param ($ScriptRoot)
+        "ScriptRoot: $ScriptRoot Runspace:$([Runspace]::DefaultRunspace.Id)" | Write-Host
+
+        # Functions and variables defined here become available in each runspace.
+        function SetPause($SyncHash, $IsPaused) {
+            $SyncHash.isPaused = $IsPaused
+            if ($IsPaused) {
+                $SyncHash.pauseButton.Content = $SyncHash.playIcon
+            } else {
+                $SyncHash.pauseButton.Content = $SyncHash.pauseIcon
+            }
         }
-    })
+    }
+
+    # RunspaceCount defines the number of event callbacks that can run in parallel at a time.
+    Set-WUIRunspacePoolOption -RunspaceCount 3 -InitializationScript $runspaceInitializationScript -InitializationScriptArgumentList $PSScriptRoot
+
+    # Dot source the initialization script here to make the functions available in the default runspace too.
+    . $runspaceInitializationScript $PSScriptRoot
+
+    $win = [Window]::new()
+    $win.Title = 'Multiple ProgressBars'
+    $win.AppWindow.TitleBar.PreferredTheme = 'UseDefaultAppMode'
+    $win.AppWindow.ResizeClient(420, 200)
+
+    $win.AddClosed({
+            # $win.WaitForClosed() waits until all the event callbacks associated with the window finish so
+            # clear the pause state to resume the event callbacks.
+            foreach ($syncHash in $script:syncHashList) {
+                SetPause $syncHash $false
+            }
+        })
+
+
+    $panel = [StackPanel]::new()
+    $panel.Margin = 16
+    $panel.Spacing = 16
+    $panel.Orientation = 'Vertical'
+    $panel.Children.Add((CreateProgressBarSet))
+    $panel.Children.Add((CreateProgressBarSet))
+    $panel.Children.Add((CreateProgressBarSet))
+
+    $win.Content = $panel
+    $win.Activate()
+    $win.WaitForClosed()
+}
 
 function CreateProgressBarSet() {
     $syncHash = [Hashtable]::Synchronized(@{})
@@ -59,8 +76,8 @@ function CreateProgressBarSet() {
     $pauseButton.IsEnabled = $false
     $pauseCallback = {
         # This runs in the default runspace.
-        param ($syncHash)
-        SetPause $syncHash (-not $syncHash.isPaused)
+        param ($SyncHash)
+        SetPause $SyncHash (-not $SyncHash.isPaused)
     }
     $pauseButton.AddClick($pauseCallback, $syncHash)
     $syncHash.pauseButton = $pauseButton
@@ -68,7 +85,7 @@ function CreateProgressBarSet() {
 
     $startButton = [Button]::new()
     $startButton.Content = 'Start'
-    $startButton.Style = $resources['AccentButtonStyle']
+    $startButton.Style = $script:resources['AccentButtonStyle']
 
     $startCallback = [EventCallback]::new()
     $startCallback.DisabledControlsWhileProcessing = $startButton
@@ -76,19 +93,19 @@ function CreateProgressBarSet() {
     $startCallback.ArgumentList = $syncHash
     $startCallback.ScriptBlock = {
         # This runs in the runspace pool.
-        param ($syncHash)
-        SetPause $syncHash $false
-        $syncHash.pauseButton.IsEnabled = $true
+        param ($SyncHash)
+        SetPause $SyncHash $false
+        $SyncHash.pauseButton.IsEnabled = $true
 
         1..100 | ForEach-Object {
-            while ($syncHash.isPaused) {
+            while ($SyncHash.isPaused) {
                 Start-Sleep -Milliseconds 20
             }
-            $syncHash.progressBar.Value = $_
+            $SyncHash.progressBar.Value = $_
             Start-Sleep -Milliseconds 20
         }
 
-        $syncHash.pauseButton.IsEnabled = $false
+        $SyncHash.pauseButton.IsEnabled = $false
     }
     $startButton.AddClick($startCallback)
 
@@ -101,14 +118,4 @@ function CreateProgressBarSet() {
     $panel
 }
 
-$panel = [StackPanel]::new()
-$panel.Margin = 16
-$panel.Spacing = 16
-$panel.Orientation = 'Vertical'
-$panel.Children.Add((CreateProgressBarSet))
-$panel.Children.Add((CreateProgressBarSet))
-$panel.Children.Add((CreateProgressBarSet))
-
-$win.Content = $panel
-$win.Activate()
-$win.WaitForClosed()
+Main
