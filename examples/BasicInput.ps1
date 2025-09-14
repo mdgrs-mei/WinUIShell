dca3dc588bc5b48cdac0a10004b7113a32def54d
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2025-09-14 21:53:55 +0900

    Add BasicInput example

---
+++ examples/BasicInput.ps1
@@ -0,0 +1,36 @@
+using namespace WinUIShell
+if (-not (Get-Module WinUIShell)) {
+    Import-Module WinUIShell
+}
+
+$win = [Window]::new()
+$win.SystemBackdrop = [DesktopAcrylicBackdrop]::new()
+$win.AppWindow.ResizeClient(420, 420)
+
+$toggleSwitch = [ToggleSwitch]::new()
+$toggleSwitch.Header = 'ToggleSwitch'
+$toggleSwitch.AddToggled({
+        Write-Host "Toggled [$($toggleSwitch.IsOn)]"
+    })
+
+$comboBox = [ComboBox]::new()
+$comboBox.Header = 'ComboBox'
+$comboBox.Description = 'Description'
+$comboBox.PlaceholderText = 'Placeholder'
+$comboBox.Items.Add('Apple')
+$comboBox.Items.Add('Banana')
+$comboBox.Items.Add('Orange')
+$comboBox.AddSelectionChanged({
+        Write-Host "Selection changed to [$($comboBox.SelectedItem)]"
+    })
+
+$panel = [StackPanel]::new()
+$panel.Margin = 32
+$panel.Spacing = 12
+
+$panel.Children.Add($toggleSwitch)
+$panel.Children.Add($comboBox)
+
+$win.Content = $panel
+$win.Activate()
+$win.WaitForClosed()
