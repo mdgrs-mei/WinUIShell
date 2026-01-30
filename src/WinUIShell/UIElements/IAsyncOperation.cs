8137c712272056f0242890b727d47ef1977e5241
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2026-01-30 22:23:12 +0900

    Add WaitForCompleted for IAsyncOperation

---
+++ src/WinUIShell/UIElements/IAsyncOperation.cs
@@ -0,0 +1,28 @@
+﻿using WinUIShell.Common;
+
+namespace WinUIShell.Windows.Foundation;
+
+public partial interface IAsyncOperation<TResult> : IAsyncInfo
+{
+    TResult WaitForCompleted();
+}
+
+public partial class IAsyncOperationImpl<TResult> : IAsyncOperation<TResult>
+{
+    public TResult WaitForCompleted()
+    {
+        while (true)
+        {
+            if (Status is
+                AsyncStatus.Completed or
+                AsyncStatus.Error or
+                AsyncStatus.Canceled)
+            {
+                return GetResults();
+            }
+
+            Engine.Get().UpdateRunspace();
+            Thread.Sleep(Constants.ClientCommandPolingIntervalMillisecond);
+        }
+    }
+}
