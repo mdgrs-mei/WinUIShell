81fc2d32a86a9eda456dc93a99b61b13d00c0562
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2026-02-11 20:22:59 +0900

    Remove AsyncOperationMethods

--- src/WinUIShell/UIElements/IAsyncOperation.cs
+++ src/WinUIShell/UIElements/IAsyncOperation.cs
@@ -1,9 +1,22 @@
-﻿namespace WinUIShell.Windows.Foundation;
+﻿using WinUIShell.Common;
+namespace WinUIShell.Windows.Foundation;
 
 public partial interface IAsyncOperation<TResult> : IAsyncInfo
 {
     TResult WaitForCompleted()
     {
-        return AsyncOperationMethods.WaitForCompleted(this);
+        while (true)
+        {
+            if (Status is
+                AsyncStatus.Completed or
+                AsyncStatus.Error or
+                AsyncStatus.Canceled)
+            {
+                return GetResults();
+            }
+
+            Engine.Get().UpdateRunspace();
+            Thread.Sleep(Constants.ClientCommandPolingIntervalMillisecond);
+        }
     }
 }
