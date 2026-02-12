dc7ee3091c9797337cdb4fe067ccd0aed19d0279
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2026-02-12 22:05:59 +0900

    Add radio buttons to BasicInput example

--- examples/BasicInput.ps1
+++ examples/BasicInput.ps1
@@ -28,12 +28,32 @@ $comboBox.AddSelectionChanged({
         Write-Host "Selection changed to [$($comboBox.SelectedItem)]"
     })
 
+$radioButtons = [RadioButtons]::new()
+$radioButtons.Header = 'Radio Buttons'
+$radioButton1 = [RadioButton]::new()
+$radioButton2 = [RadioButton]::new()
+$radioButton3 = [RadioButton]::new()
+$radioButton1.Content = 'Option1'
+$radioButton2.Content = 'Option2'
+$radioButton3.Content = 'Option3'
+$radioButtons.Items.Add($radioButton1)
+$radioButtons.Items.Add($radioButton2)
+$radioButtons.Items.Add($radioButton3)
+$radioButtons.AddSelectionChanged({
+        param ($argumentList, $s, $selectionChangedEventArgs)
+        # This event seems to be raised twice for checked and unchecked. Only print for the checked event.
+        if ($selectionChangedEventArgs.AddedItems[0]) {
+            Write-Host "Selection changed to [$($radioButtons.SelectedItem.Content)]"
+        }
+    })
+
 $panel = [StackPanel]::new()
 $panel.Margin = 32
 $panel.Spacing = 12
 
 $panel.Children.Add($toggleSwitch)
 $panel.Children.Add($comboBox)
+$panel.Children.Add($radioButtons)
 
 $win.Content = $panel
 $win.Activate()
